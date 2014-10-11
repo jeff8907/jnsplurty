b278cf42542b2a43203025077b680b67a06ceff6
Jeff Nicholas <jeff8907@gmail.com>
2014-10-11 14:10:55 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jnsplurty::Application.config.secret_key_base = '7e0a9b7ffa3a4bbe9696aa8400e90641af9c33a74d1de81c137e0028062f7e0325b05431970900754b26c88440328dbc05d36b5fabe5518ef273d24f188dad8c'
